12d7385904e70233da59dc8eb94d567eccdf4af8
Bill Xiong <billxiong99@gmail.com>
2018-10-06 15:53:17 -0700

    forgot to correct for uppercase, rip.

--- 05.js
+++ 05.js
@@ -15,5 +15,5 @@ function totalCells(){
 }
 
 function convertColumn(coordinate){
-  return coordinate.charCodeAt(0) - 65;//You're supposed to do this with arrays, but it was easier just to return ascii values. Apparently there is a very very long video tutorial on how to solve on the forums.
+  return coordinate.toUpperCase().charCodeAt(0) - 65;//You're supposed to do this with arrays, but it was easier just to return ascii values. Apparently there is a very very long video tutorial on how to solve on the forums.
 }
