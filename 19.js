333e87309c96e85924671222f2adb7fadb92ffbf
Bill Xiong <billxiong99@gmail.com>
2018-10-20 14:09:22 -0700

    Create 19.js

---
+++ 19.js
@@ -0,0 +1,104 @@
+function countRows() {
+  return (GRID.length);  // TODO: Refactor with more oomph..
+}
+
+function countColumns() {
+  return GRID.length ? GRID[0].length : 0;
+}
+
+function gridSize() {
+  return (countColumns() + " x " + countRows());
+}
+
+function totalCells() {
+  return (countRows() * countColumns());
+}
+
+function convertColumn(coordinate) {
+  return coordinate.toUpperCase().charCodeAt(0) - 65;
+}
+
+function lightCell(coordinate) {
+  return GRID[parseInt(coordinate[1]) - 1][convertColumn(coordinate)] ? GRID[parseInt(coordinate[1]) - 1][convertColumn(coordinate)] : false;
+}
+
+function isRock(coordinate) {
+  return (lightCell(coordinate) == "^");
+}
+
+function isCurrent(coordinate) {
+  return (lightCell(coordinate) == "~");
+}
+
+function isShip(coordinate) {
+  return (lightCell(coordinate) == "v");
+}
+
+function lightRow(row) {
+  return GRID[row - 1];
+}
+
+function lightColumn(column) {
+  return GRID.map(function (value, index) { return value[convertColumn(column)]; });
+}
+
+function allRocks() {
+    var results = [];
+    GRID.map((row, rowIndex) => {
+        row.map((colItem, colIndex) => {
+            if (colItem === "^" ) {
+                results.push(String.fromCharCode(colIndex + 65) + (rowIndex + 1));
+                //don't forget your offsets people
+            }
+            return;
+        });
+    })
+    return results;
+}
+
+function allCurrents() {
+    var results = [];
+    GRID.map((row, rowIndex) => {
+        row.map((colItem, colIndex) => {
+            if (colItem === "~" ) {
+                results.push(String.fromCharCode(colIndex + 65) + (rowIndex + 1));
+                //don't forget your offsets people
+            }
+            return;
+        });
+    })
+    return results;
+}
+
+function allShips() {
+    var results = [];
+    GRID.map((row, rowIndex) => {
+        row.map((colItem, colIndex) => {
+            if (colItem === "v" ) {
+                results.push(String.fromCharCode(colIndex + 65) + (rowIndex + 1));
+                //don't forget your offsets people
+            }
+            return;
+        });
+    })
+    return results;
+}
+
+function firstRock(){
+  return allRocks()[0];
+}
+
+function firstCurrent(){
+  return allCurrents()[0];
+}
+
+function shipReport(){//Transport Canada doesn't seem to be asking for a lot.
+  return ([allShips()[0],allShips()[allShips().length-1]]);
+}
+
+var howDangerous = (coordinate) => ({'^': 100, '~': 50})[lightCell(coordinate)] || 0; //fancy code is often better, thanks Sam (Update by Sam: I wouldn't use this in production haha) 
+
+function percentageReport(){
+ return [(100*allRocks().length/totalCells()).toFixed(2),(100*allCurrents().length/totalCells()).toFixed(2)];
+ //corner cases, what corner cases? ¯\_(ツ)_/¯
+}
